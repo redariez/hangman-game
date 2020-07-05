91fc0c837173a03bedef55b2098a6c9f05ec1cde
ade bakari <redariez@gmail.com>
2020-07-05 12:16:11 -0700

    display words function

---
+++ hangman/script.js
@@ -0,0 +1,78 @@
+const wordEl = document.getElementById('word');
+const wrongLettersEl = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-button');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
+
+const figureParts = document.querySelectorAll('.figure-part');
+
+const words = ['application', 'programming', 'interface', 'wizard'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const correctLetters = [];
+const wrongLetters = [];
+
+// Show hidden word
+function displayWord() {
+  wordEl.innerHTML = `
+    ${selectedWord
+      .split('')
+      .map(
+        letter => `
+          <span class="letter">
+            ${correctLetters.includes(letter) ? letter : ''}
+          </span>
+        `
+      )
+      .join('')}
+  `;
+  const innerWord = wordEl.innerText.replace(/\n/g, '');
+  
+  if(innerWord === selectedWord) {
+      finalMessage.innerText = 'congradulations! You won! ';
+      popup.style.display = 'flex';
+  }
+}
+
+// Update the wrong letters
+function updateWrongLettersEl() {
+    console.log('Update wrong');
+}
+
+//Show notification
+function showNotification() {
+    notification.classList.add('show');
+
+    setTimeout(() => {
+        notification.classList.add('show');
+    }, 2000);
+}
+
+// keydown letter press
+window.addEventListener('keydown', e => {
+   // console.log(e.keyCode);
+   if(e.keyCode >= e.keycode <= 90) {
+       const letter = e.key;
+
+       if(selectedWord.includes(letter)) {
+         if(!correctedLetters.includes(letter)) {
+             correctedLetters.push(letter);
+
+            displayWord();
+         } else {
+             showNotification();
+         }
+     } else {
+         if(!wrongLetters.includes(letter)) {
+             wrongLetters.push(letter);
+
+             updateWrongLettersEl();
+         } else {
+             showNotification();
+         }
+     }
+   }
+});
+  displayWord();
